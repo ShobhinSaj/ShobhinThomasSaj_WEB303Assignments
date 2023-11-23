26e1c78303d34f2a93dfae572e65d1e40bed7b94
madmax117 <maxjonas117@gmail.com>
2023-11-23 16:02:08 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -7,5 +7,18 @@ $(function () {
             height: 'auto'
         });
        e.preventDefault();
+       $('#modal-content').html($content);
+        $('#image-modal').css('display', 'flex');
+        $('#image-modal').click(function (e) {
+        if (e.target.id === 'image-modal') {
+            $(this).css('display', 'none');
+        }
     });
+
+    
+    $('#close-modal').click(function () {
+        $('#image-modal').css('display', 'none');
+    });
+    });
+    
 });
